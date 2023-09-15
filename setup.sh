8cebd9a2cf777580af1a7c8dd4b6f68bb2f02ee1
Fiona June Leathers <fiona.june.leathers@outlook.com>
2023-09-15 14:22:27 -0400

    added setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "CLOUD-HARVEST AWS PLUGIN SETUP"
+echo "INSTALL AWS CLI"
+# https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/root/awscliv2.zip"
+unzip /root/awscliv2.zip
+/root/aws/install
+
+which aws || echo "FAILED TO INSTALL AWS CLI TO PATH" && exit 1
+
+echo "COMPLETE"
