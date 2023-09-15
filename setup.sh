98d32a89dd455b089266f43fb3486cf06133311d
Fiona June Leathers <fiona.june.leathers@outlook.com>
2023-09-15 14:39:11 -0400

    added missing unzip -d

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ echo "CLOUD-HARVEST AWS PLUGIN SETUP"
 echo "INSTALL AWS CLI"
 # https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/root/awscliv2.zip"
-unzip /root/awscliv2.zip
+unzip /root/awscliv2.zip -d /root/
 /root/aws/install
 
 which aws || echo "FAILED TO INSTALL AWS CLI TO PATH" && exit 1
