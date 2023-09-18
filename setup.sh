f6e421903ec76b3f5320001b1b236fbc06bc0340
Fiona June Leathers <fiona.june.leathers@outlook.com>
2023-09-18 12:08:22 -0400

    moved aws cli setup to the client aws plugin

--- setup.sh
+++ setup.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-echo "CLOUD-HARVEST AWS PLUGIN SETUP"
-echo "INSTALL AWS CLI"
-# https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/root/awscliv2.zip"
-unzip /root/awscliv2.zip -d /root/
-/root/aws/install
+#echo "CLOUD-HARVEST API AWS PLUGIN SETUP"
 
-which aws || echo "FAILED TO INSTALL AWS CLI TO PATH" && exit 1
-
-echo "COMPLETE"
+#echo "COMPLETE"
