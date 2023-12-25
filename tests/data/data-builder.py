bf45f145fbfbb42f2d315d53f03dbc9bd3e68c7b
Fiona June Leathers <fiona.june.leathers@outlook.com>
2023-12-24 23:54:04 -0500

    rewrote to pull all services and commands

--- tests/data/data-builder.py
+++ tests/data/data-builder.py
@@ -1,116 +1,256 @@
 #!/bin/env python
 
-def main(services_file: str, count: int, max_workers: int):
+from subprocess import Popen, PIPE
+
+invalid_result_keys = ('Marker', 'NextToken')
+data_gathering_command_prefixes = ('describe', 'get', 'list')
+
+
+def main(aws: str, count: int, max_workers: int, no_cache: bool):
     """
     Converts API calls into test records and prints them to stdout.
-    :param services_file: YAML file containing the services and their respective api calls/depths
+    :param aws: path to the aws cli tool
     :param count: the number of test records to create per service/type
     :param max_workers: maximum number of threads to run when creating test records
+    :param no_cache: do not use cached service, command, or template information; retrieve new directives from the aws cli
     :return: outputs a JSON file to the screen
     """
+    from datetime import datetime, timezone
 
-    with open(services_file, 'r') as services_stream:
-        from yaml import load, FullLoader
-        services = load(services_stream, Loader=FullLoader)
+    start_time = datetime.now(tz=timezone.utc).timestamp()
 
-    from flatten_json import flatten, unflatten_list
-    from json import loads
-    from os.path import expanduser
-    from os import environ
-    from shutil import which
-    from subprocess import Popen, PIPE
+    from os.path import exists
+    from os import mkdir
+    if not exists('./cache'):
+        mkdir('./cache')
 
-    aws_binary = expanduser(which('aws', path=environ.get('PATH')))
+    from concurrent.futures import ThreadPoolExecutor, wait, ALL_COMPLETED
+    with ThreadPoolExecutor(max_workers=max_workers) as pool:
+        futures = [pool.submit(get_commands, service, no_cache) for service in get_services(no_cache=no_cache)]
 
-    z = 0
-    s = len(set(['.'.join(f.split('.')[0:2])
-                 for f in flatten(services, separator='.')]))
+        wait(futures, return_when=ALL_COMPLETED)
 
-    for _service in services.keys():
-        for _type in services[_service].keys():
-            z += 1
-            print(f'\nbuild: {_service}.{_type}: {z} / {s}')
+    commands = []
+    [commands.extend(f.result()) for f in futures]
 
-            service_type = services[_service][_type]
+    with ThreadPoolExecutor(max_workers=max_workers) as pool:
+        futures = [pool.submit(get_outputs, aws, command, count, no_cache) for command in commands]
+                   # if any([command.split('.')[1].lower().startswith(s) for s in data_gathering_command_prefixes])]
 
-            c = (' '.join([
-                aws_binary,
-                _service,
-                service_type['command'],
-                '--region us-east-1',
-                '--generate-cli-skeleton output'
-            ]))
+        wait(futures, return_when=ALL_COMPLETED)
 
-            print(f'cmd  : {c}')
+    # results = {f.result()[0]: f.result()[1] for f in futures}
 
-            process = Popen(args=c.split(' '),
-                            stdout=PIPE)
+    end_time = datetime.now(tz=timezone.utc).timestamp()
 
-            p = process.communicate()
+    print(f'done in {end_time - start_time} seconds')
 
-            template = loads(p[0].decode('utf8'))
+    return
 
-            result_key = [k for k in template.keys() if k != 'Marker'][0]
 
-            skeleton_file = f'./skeletons/{_service}.{_type}.json'
+def get_services(no_cache: bool) -> list:
+    from os.path import exists
+    from json import loads, dumps
 
-            import hashlib
-            from os.path import exists
-            if exists(skeleton_file):
-                old_skeleton_md5 = hashlib.md5(open(skeleton_file, 'rb').read()).hexdigest()
+    services_file = './cache/services.json'
+    if exists(services_file) and no_cache is False:
+        with open(services_file, 'r') as services_cache:
+            services = loads(services_cache.read())
+            print(f'loaded: {services_file}')
+
+    else:
+        process = Popen(args=['aws', 'help'], stdout=PIPE)
+        process = process.communicate()[0].decode('utf8').split('\n')
+
+        services = []
+        read_services = False
+        for r in process:
+            row = r.strip()
+
+            if 'AVAILABLE SERVICES' in row:
+                read_services = True
+
+            elif read_services is True and row != '':
+                if 'SEE ALSO' in row:
+                    break
+
+                else:
+                    service = row.split(' ')[-1]
+                    services.append(service)
+
+        with open('./cache/services.json', 'w') as services_cache:
+            services_cache.write(dumps(services, default=str, indent=4))
+
+    return services
+
+
+def get_commands(service: str, no_cache: bool) -> list:
+    from os.path import exists
+    from json import dumps, loads
+
+    commands_cache_file = f'./cache/{service}.commands.json'
+
+    if exists(commands_cache_file) and no_cache is False:
+        with open(commands_cache_file, 'r') as commands_cache:
+            result = loads(commands_cache.read())
+            print(f'loaded: {commands_cache_file}')
+
+    else:
+        from subprocess import Popen, PIPE
+        process = Popen(args=['aws', service, 'help'], stdout=PIPE)
+        process = process.communicate()[0].decode('utf8').split('\n')
+
+        result = []
+
+        read_commands = False
+        for row in process:
+
+            if 'AVAILABLE COMMANDS' in row:
+                read_commands = True
+
+            elif read_commands is True and row != '' and not row.endswith('()'):
+                command = row.split(' ')[-1]
+                service_command = f'{service}.{command}'
+                print(service_command)
+
+                result.append(service_command)
+
+        with open(commands_cache_file, 'w') as commands_cache:
+            commands_cache.write(dumps(result, default=str, indent=4))
+
+    return result
+
+
+def get_outputs(aws: str, service_command: str, count: int, no_cache: bool) -> tuple:
+    service, command = service_command.split('.', maxsplit=1)
+    from datetime import datetime, timezone
+
+    start_time = datetime.now(tz=timezone.utc).timestamp()
+
+    from json import dumps, loads
+    from os.path import exists
+
+    command_output_file = f'./cache/{service}.{command}.output.json'
+
+    if exists(command_output_file) and no_cache is False:
+        with open(command_output_file, 'r') as command_output_stream:
+            return loads(command_output_stream.read())
+
+    else:
+        from json import loads
+        from hashlib import md5
+
+        def run_command(*args) -> tuple:
+            required = []
+            while True:
+                r = [f'{r}=""' for r in required]
+                c = ' '.join([*args, *r])
+
+                process = Popen(args=[*args, *r], stdout=PIPE, stderr=PIPE)
+                output_raw = process.communicate()[0]
+
+                # RETRY: this command requires an input of some kind
+                if b'aws: error: the following arguments are required:' in output_raw:
+                    from re import findall
+                    required = findall(b'--.*\\w', output_raw)
+                    print(f'{c}: WARN: added: {required}')
+                    continue
+
+                # ERROR: this command won't give us back a skeleton
+                elif b'Unknown options: --generate-cli-skeleton' in output_raw:
+                    print(f'{c}: ERROR: --generate-cli-skeleton not supported')
+                    break
+
+                # hopefully the output we want
+                else:
+                    return output_raw, required
+
+        input_raw, input_required = run_command(aws, service, command,
+                                                '--region=us-east-1', '--generate-cli-skeleton')
+
+        output_raw, output_required = run_command(aws, service, command,
+                                                  '--region=us-east-1', '--generate-cli-skeleton', 'output')
+
+        output_json = loads(output_raw.decode('utf8'))
+
+        result_key = [k for k in output_json.keys() if k not in invalid_result_keys]
+        result_key = result_key[0] if len(result_key) > 0 else None
+
+        print(f'{service_command}: '
+              f'{"WARN" if any([len(input_raw) == 0, 
+                                len(output_raw) == 0,
+                                result_key is None]) else "INFO"}: '
+              f'{len(input_raw)} | '
+              f'{len(output_raw)} | '
+              f'{result_key}')
+
+        result = {
+            'input': {
+                'template': loads(input_raw.decode('utf8')),
+                'required': input_required,
+                'md5': md5(input_raw).hexdigest()
+            },
+            'output': {
+                'template': output_json.get(result_key),
+                'required': output_required,
+                'md5': md5(output_raw).hexdigest(),
+                'randomized': create_random_data(template=output_json[result_key], count=count) if result_key else []
+            },
+            'meta': {
+                'start': start_time,
+                'end': datetime.now(tz=timezone.utc).timestamp(),
+                'duration': datetime.now(tz=timezone.utc).timestamp() - start_time
+            }
+        }
+
+        with open(command_output_file, 'w') as command_output_stream:
+            command_output_stream.write(dumps(result))
+
+        print(f'{service}.{command}: done in {result["meta"]["duration"]} seconds -> {command_output_file}')
+
+        return service_command, result
+
+
+def create_random_data(template: dict, count: int) -> list:
+    result = []
+
+    import random
+    import string
+
+    letters = string.ascii_lowercase
+
+    from flatten_json import flatten
+    flat_template = flatten(template, separator='.')
+
+    for i in range(count):
+        r = {}
+        for k, v in flat_template.items():
+            if isinstance(v, str):
+                r[k] = ''.join(random.choice(letters) for i in range(16))
 
             else:
-                old_skeleton_md5 = None
+                r[k] = v
 
-            with open(skeleton_file, 'w') as skeleton_stream:
-                from pprint import pformat
-                skeleton_stream.write(pformat(template[result_key]))
-                skeleton_stream.write('\n')
-                print(f'wrote: {skeleton_file}')
+        result.append(r)
 
-            if old_skeleton_md5 is not None:
-                new_skeleton_md5 = hashlib.md5(open(skeleton_file, 'rb').read()).hexdigest()
-
-                if new_skeleton_md5 != old_skeleton_md5:
-                    print(f'WARNING: {_service}.{_type} md5 do not match: {skeleton_file}: {old_skeleton_md5} / {new_skeleton_md5}')
-
-            if isinstance(template[result_key], list):
-                flat_template = flatten(template[result_key][0], separator='.')
-
-            else:
-                flat_template = flatten(template[result_key], separator='.')
-
-            import random
-            import string
-            from copy import deepcopy
-            results = []
-
-            for x in range(0, count):
-                dc_template = deepcopy(flat_template)
-                for key, value in dc_template.items():
-                    if isinstance(value, str):
-                        letters = string.ascii_lowercase
-                        dc_template[key] = ''.join(random.choice(letters) for i in range(10))
-
-                results.append(unflatten_list(dc_template, separator='.'))
-
-            output_file = f'./outputs/{_service}.{_type}.json'
-            with open(output_file, 'w') as output_stream:
-                output_stream.writelines([pformat(o) + '\n' for o in results])
-
-            print(f'wrote: {output_file}')
+    return result
 
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
 
     parser = ArgumentParser()
-    parser.add_argument('-f', '--services-file', default='./services.yaml',
-                        help='Location of the services.yaml')
-    parser.add_argument('-c', '--count', default=50,
-                        help='Number of test records to create per service.')
-    parser.add_argument('-w', '--max-workers', default=10, type=int,
-                        help='Number of data generation threads to run at once.')
+    parser.add_argument('-c', '--count', default=10,
+                        help='Number of test records to create per service. (default: 10)')
+    parser.add_argument('-w', '--max-workers', default=16, type=int,
+                        help='Number of data generation threads to run at once. (default: 16)')
+    parser.add_argument('--no-cache', action='store_true', help='do not use the services or command cache')
 
-    args = vars(parser.parse_args())
-    main(**args)
+    arguments = vars(parser.parse_args())
+
+    from os.path import expanduser
+    from os import environ
+    from shutil import which
+    aws_binary = expanduser(which('aws', path=environ.get('PATH')))
+
+    main(aws=aws_binary, **arguments)
