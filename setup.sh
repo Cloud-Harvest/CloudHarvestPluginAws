d6754880349fcb2f3a78bec0391caa231dad7b55
Fiona June Leathers <fiona.june.leathers@outlook.com>
2024-04-29 17:25:53 -0400

    include saml2aws on install

--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-#echo "CLOUD-HARVEST API AWS PLUGIN SETUP"
+version="2.36.15"
+file_name="saml2aws_${version}_linux_amd64.tar.gz"
+
+echo "CLOUD-HARVEST API AWS PLUGIN SETUP"
+
+echo "Installing saml2aws..."
+
+wget -P /root/ "https://github.com/Versent/saml2aws/releases/download/v${version}/${file_name}"
+tar -xvf "/root/${file_name}" -C /root/
+mv -v /root/saml2aws /usr/local/bin/saml2aws
 
 echo "COMPLETE"
