6c07fbf1e93dc624f8195698a86dfb872719187a
Fiona June Leathers <fiona.june.leathers@outlook.com>
2024-05-13 15:55:09 -0400

    updated package_data

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ with open('requirements.txt') as f:
 
 config = dict(packages=find_packages(include=['CloudHarvestPluginAws', 'CloudHarvestPluginAws.*']),
               install_requires=requirements,
+              package_data={'CloudHarvestPluginAws': ['reports/**/*.yaml']},
               include_package_data=True)
 
 config = config | meta
