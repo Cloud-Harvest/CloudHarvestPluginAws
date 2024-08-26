aa52f98eef6d68f6762201a131ade8f96b056134
Fiona June Leathers <fiona.june.leathers@outlook.com>
2024-08-26 11:20:00 -0400

    moved functions into different files
    added docs
    
    added more methods for interacting with AWS credentials

--- CloudHarvestPluginAws/authenticators/iam.py
+++ CloudHarvestPluginAws/authenticators/iam.py
@@ -14,6 +14,8 @@ class Credentials:
         'by_arn': {}
     }
 
+    auto_refresh_thread = None
+
     @staticmethod
     def add(credential: 'Credential', override_profile_name: str = None):
         """
@@ -83,18 +85,66 @@ class Credentials:
             arn (str, optional): The ARN. Default is None.
         """
 
-        if profile_name:
-            return Credentials.index['by_profile_name'].get(profile_name)
+        result = None
+        # Look up the credential by profile name, account ID, or ARN. Return the first matching index.
+        if profile_name and Credentials.index['by_profile_name'].get(profile_name):
+            result = Credentials.index['by_profile_name'].get(profile_name)
 
-        elif account_id:
-            return Credentials.index['by_account_id'].get(account_id)
+        elif account_id and Credentials.index['by_account_id'].get(account_id):
+            result = Credentials.index['by_account_id'].get(account_id)
 
-        elif arn:
-            return Credentials.index['by_arn'].get(arn)
+        elif arn and Credentials.index['by_arn'].get(arn):
+            result = Credentials.index['by_arn'].get(arn)
 
+        return result
+
+    @staticmethod
+    def start_auto_refresh(worker_refresh_rate: int = 60):
+        """
+        Start the auto-refresh thread for the index.
+        """
+
+        from threading import Thread
+        from time import sleep
+
+        def auto_refresh():
+            while True:
+                for profile_name, credential in Credentials.index['by_profile_name'].items():
+                    if credential.is_expired:
+                        credential.lookup_credentials(requesting_profile_name='default')
+
+                sleep(worker_refresh_rate)
+
+        Credentials.auto_refresh_thread = Thread(target=auto_refresh)
+        Credentials.auto_refresh_thread.start()
 
 class Credential:
-    def __init__(self):
+    """
+    A class to manage AWS credentials. The Credential class is used to store and manage AWS credentials for Harvest.
+    These values are not assigned on instantiation because there are many paths to populating credentials. Instead,
+    the Credential class provides methods to look up and set the credentials.
+
+    Attributes:
+        aws_access_key_id (str): The AWS access key ID.
+        aws_role_arn (str): The Amazon Resource Name (ARN) of the AWS role.
+        aws_role_session_name (str): The session name for the AWS role.
+        aws_secret_access_key (str): The AWS secret access key.
+        aws_session_token (str): The AWS session token.
+        account_id (str): The AWS account ID.
+        account_name (str): The name of the AWS account.
+        aws_role_duration_seconds (int): The duration, in seconds, for which the role credentials are valid.
+        token_create_datetime (datetime): The datetime when the session token was created.
+        requestor (str): The ARN of the role that requested the credentials.
+        auto_refresh (bool): If True, the credentials are automatically refreshed via the Credentials start_auto_refresh() method.
+
+    Properties:
+        boto_session_map (dict): The Boto3 session map for the credentials.
+        has_credentials (bool): Check if the credentials have been set.
+        is_expired (bool): Check if the credentials are expired.
+        profile_name (str): The profile name for the credentials.
+        role_name (str): The role name for the credentials.
+    """
+    def __init__(self, **kwargs):
         # Identities
         self.aws_role_arn = None
         self.aws_role_session_name = None
@@ -103,13 +153,20 @@ class Credential:
         self.aws_access_key_id = None
         self.aws_secret_access_key = None
         self.aws_session_token = None
+        self.aws_secret_expiration = None
 
         # Data
         self.account_id = None
         self.account_name = None
         self.aws_role_duration_seconds = None
         self.token_create_datetime = None
+        self.requestor = None
+        self.auto_refresh = False
 
+        # Set the attributes from the keyword arguments.
+        for key, value in kwargs.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
 
     @property
     def boto_session_map(self) -> dict:
@@ -149,13 +206,19 @@ class Credential:
     @property
     def is_expired(self) -> bool or None:
         """
-        Check if the credentials are expired. To determine if the credentials are expired, the token_create_datetime and
-        aws_role_duration_seconds must be set. Otherwise, the result is always None.
+        Check if the credentials are expired. We check several conditions to determine if the credentials are expired.
+        1. If the aws_secret_expiration is set and the expiration is less than the current time, the credentials are expired.
+        2. If the aws_role_duration_seconds and token_create_datetime are set and the duration has passed, the credentials are expired.
+
+        If none of these conditions are met, the credentials are not expired.
         """
 
         from datetime import datetime, timezone
 
-        if self.aws_role_duration_seconds and self.token_create_datetime:
+        if self.aws_secret_expiration:
+            return self.aws_secret_expiration < datetime.now(timezone.utc)
+
+        elif self.aws_role_duration_seconds and self.token_create_datetime:
             return self.token_create_datetime + self.aws_role_duration_seconds < datetime.now(timezone.utc)
 
         else:
@@ -198,21 +261,6 @@ class Credential:
 
         finally:
             return None
-
-    def lookup_duration(self, requestor: dict) -> int:
-        """
-        Look up the role duration. If the role duration is not set, the duration is fetched from the AWS API and set.
-        """
-
-        if not self.aws_role_duration_seconds:
-            from boto3 import Session
-            session = Session(**Credentials.get(**requestor).boto_session_map)
-            client = session.client('iam')
-            result = client.get_role(RoleName=self.role_name).get('Role').get('MaxSessionDuration')
-
-            self.aws_role_duration_seconds = result
-
-        return self.aws_role_duration_seconds
 
     def lookup_credentials(self,
                            requesting_profile_name: str = None,
@@ -266,17 +314,40 @@ class Credential:
                 sleep(.5)
 
             else:
+                from datetime import datetime, timezone
+
                 self.aws_session_token = response['Credentials']['SessionToken']
                 self.aws_access_key_id = response['Credentials']['AccessKeyId']
                 self.aws_secret_access_key = response['Credentials']['SecretAccessKey']
+                self.aws_secret_expiration = response['Credentials']['Expiration']
+                self.token_create_datetime = datetime.now(timezone.utc)
+                self.requestor = requesting_profile_name
 
                 return self.boto_session_map
 
-    def lookup_role_arn(self) -> str:
+
+    def lookup_duration(self, requestor: dict) -> int:
         """
-        Get the AWS role ARN from the credentials.
+        Look up the role duration. If the role duration is not set, the duration is fetched from the AWS API and set.
         """
 
+        # Only look up the role duration if it is not set.
+        if not self.aws_role_duration_seconds:
+            from boto3 import Session
+            session = Session(**Credentials.get(**requestor).boto_session_map)
+            client = session.client('iam')
+            result = client.get_role(RoleName=self.role_name).get('Role').get('MaxSessionDuration')
+
+            self.aws_role_duration_seconds = result
+
+        return self.aws_role_duration_seconds
+
+    def lookup_role_arn(self) -> str:
+        """
+        Get the AWS role ARN from STS if it is not set. Otherwise, return the set role ARN.
+        """
+
+        # Only look up the role ARN if it is not set.
         if self.aws_role_arn:
             return self.aws_role_arn
 
@@ -293,3 +364,58 @@ class Credential:
         return self.account_id
 
 
+def lookup_assumable_roles(profile_name = 'default') -> List[str]:
+    """
+    Look up the assumable roles for a profile. This function is intended to work with the default role attached to an
+    EC2 instance or similar AWS service which can assume roles. Once the assumable roles are found, use the
+    lookup_credentials method to assume the role and get the tokens.
+
+    Args:
+        profile_name (str, optional): The profile name. Default is 'default'.
+
+    Example:
+        >>> # This example follows the code chain necessary to get the credentials from an EC2 instance, then assume
+        >>> # all the credentials that the instance can assume.
+        >>> admin_credentials = Credential()
+        >>> admin_credentials.lookup_credentials()
+        >>>
+        >>> # Look up the assumable roles.
+        >>> arns = lookup_assumable_roles(profile_name=admin_credentials.profile_name)
+        >>> [
+        >>>     'arn:aws:iam::123456789012:role/MyRole',
+        >>>     'arn:aws:iam::123456789012:role/MyOtherRole'
+        >>> ]
+        >>>
+        >>> for arn in arns:
+        >>>     # Create Credential objects for each Arn
+        >>>     credential = Credential()
+        >>>     credential.aws_role_arn = arn
+        >>>
+        >>>     # Get the credentials for the role
+        >>>     credential.lookup_credentials(requesting_profile_name=admin_credentials.profile_name)
+        >>>
+        >>>     # Add the credentials to the index
+        >>>     Credentials.add(credential)
+    """
+
+    from boto3 import Session
+
+    credential = Credentials.get(profile_name=profile_name)
+    session = Session(**credential.boto_session_map)
+    client = session.client('sts')
+
+    # List all roles
+    roles = client.list_roles()
+
+    assumable_roles = []
+
+    # Check the trust relationships of each role
+    for role in roles['Roles']:
+        assume_role_policy = role['AssumeRolePolicyDocument']
+        for statement in assume_role_policy['Statement']:
+            if statement['Effect'] == 'Allow' and 'AWS' in statement['Principal']:
+
+                # Add the role ARN to the list if the current role is allowed to assume it
+                assumable_roles.append(role['Arn'])
+
+    return assumable_roles
